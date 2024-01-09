ec63303de559c6ad468badc6b710f8e00abb29d9
DavutHaxor <davutdemir16@hotmail.com>
2024-01-09 21:46:10 +0300

    ...

--- ffmpeg_cut.sh
+++ ffmpeg_cut.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/bash
+
+# Function to convert Minute.Seconds to seconds
+convert_to_seconds() {
+    local time=$1
+    local minutes=$(echo "$time" | cut -d'.' -f1)
+    local seconds=$(echo "$time" | cut -d'.' -f2)
+
+    # Check if the input is valid
+    if [[ -z $minutes || -z $seconds || ! $minutes =~ ^[0-9]+$ || ! $seconds =~ ^[0-9]+$ ]]; then
+        echo "Invalid input format. Please provide a valid Minute.Seconds format."
+        exit 1
+    fi
+
+    local total_seconds=$((minutes * 60 + seconds))
+    echo "$total_seconds"
+}
+
+# Prompt user for input
 echo "Skip Time ||| Input File ||| Duration ||| Output File"
 read skip input duration output
 
-ffmpeg -ss $skip -i $input -c copy -t $duration $output
+# Convert skip time to seconds
+skip_seconds=$(convert_to_seconds "$skip")
+#duration_seconds=$(convert_to_seconds "$duration")
 
+# Use ffmpeg with the converted skip time in seconds
+ffmpeg -ss $skip_seconds -i "$input" -c copy -t "$duration" "$output"
